36b02fdc3cb3f9bc541729628e571527273d19ab
dannybackx <dannybackx@users.noreply.github.com>
2020-08-23 08:00:32 +0200

    Add first cut of minimal PCP (Port Control Protocol) implementation, to punch a hole in a firewall.

---
+++ kippen/esp/main/PcpClient.cpp
@@ -0,0 +1,341 @@
+/*
+ * Copyright (c) 2017, 2020 Danny Backx
+ *
+ * License (MIT license):
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy
+ *   of this software and associated documentation files (the "Software"), to deal
+ *   in the Software without restriction, including without limitation the rights
+ *   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *   copies of the Software, and to permit persons to whom the Software is
+ *   furnished to do so, subject to the following conditions:
+ *
+ *   The above copyright notice and this permission notice shall be included in
+ *   all copies or substantial portions of the Software.
+ *
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *   THE SOFTWARE.
+ *
+ * This class allows you to "punch a hole" in the NAT router/firewall typically used.
+ *
+ * A local port on the esp can be remapped to some other port number on the router.
+ * Someone accessing that port number can thereby get access to the esp even through
+ * the NAT router.
+ *
+ * Protocol used : PCP (Port Control Protocol), RFC 6887, which is a superset of
+ * NAT-PMP (NAT Port Mapping Protocol), RFC 6886.
+ *
+ * Obviously (grin) this is a minimal implementation.
+ */
+
+#include <esp_log.h>
+#include <string.h>
+#include "PcpClient.h"
+#include <netinet/in.h>
+
+static PcpClient *self;
+void pcp_task(void *);	// Forward declaration needed, as in the include file it's just a friend, not declared.
+
+PcpClient::PcpClient() {
+  sock = -1;
+  self = this;
+  task = 0;
+
+  xTaskCreate(&pcp_task, "pcp mcast", 4096, NULL, 5, &task);
+}
+
+PcpClient::~PcpClient() {
+  if (sock) {
+    close(sock);
+    sock = 0;
+  }
+  if (task) {
+    vTaskDelete(task);
+    task = 0;
+  }
+}
+
+void PcpClient::setLocalIP(in_addr_t local) {
+  ESP_LOGD("pcp", "SetLocalIP(%s)", inet_ntoa(local));
+
+  this->local = local;
+}
+
+void PcpClient::setRouter(const char *hostname) {
+  router_name = (char *)hostname;
+  router_ip = inet_addr(hostname);
+}
+
+void PcpClient::setRouter(const in_addr_t host) {
+  router_ip = host;
+}
+
+esp_err_t PcpClient::NetworkConnected(void *ctx, system_event_t *event) {
+  switch (event->event_id) {
+    case SYSTEM_EVENT_GOT_IP6:
+      break;
+
+    case SYSTEM_EVENT_STA_GOT_IP:
+      setLocalIP(event->event_info.got_ip.ip_info.ip.addr);
+
+      // pcp->setRouter("192.168.0.228");			// Acer
+      pcp->setRouter("192.168.0.1");
+      pcp->addPort(35777, 35777, IPPROTO_TCP, 1000);	// FIX ME test
+
+      break;
+
+    default:
+      break;
+  }
+  
+  return ESP_OK;
+}
+
+void PcpClient::sendPacket(const char *packet, const int len) {
+  if (sock < 0) {
+    sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_IP);
+    ESP_LOGI("pcp", "sendPacket: socket %d", sock);
+
+    struct sockaddr_in ls;
+    memset((char *)&ls, 0, sizeof(ls));
+    ls.sin_family = PF_INET;
+    ls.sin_port = htons(pcp_client_port);
+    ls.sin_addr.s_addr = INADDR_ANY;
+
+    if (bind(sock, (sockaddr *)&ls, sizeof(ls)) < 0) {
+      ESP_LOGE("pcp", "sendPacket: bind to port %d failed %d %s", pcp_client_port, errno, strerror(errno));
+    } else
+      ESP_LOGI("pcp", "sendPacket: bind to port %d ok", pcp_client_port);
+
+  }
+
+  {
+    char s[64], a[10];
+    for (int i=0; i<len; i++) {
+      if (i % 16 == 0) {
+	if (i > 0) {
+	  ESP_LOGI("pcp", "send %s", s);
+	  s[0]=0;
+	}
+        sprintf(s, "%02x : ", i);
+      }
+      sprintf(a, "%02x ", packet[i]);
+      strcat(s, a);
+    }
+    if (s[0]) {
+      ESP_LOGI("pcp", "send %s", s);
+    }
+  }
+
+  struct sockaddr_in dest;
+  dest.sin_family = AF_INET;
+  dest.sin_port = ntohs(pcp_server_port);
+  dest.sin_addr.s_addr = router_ip;
+
+  ssize_t r = sendto(sock, packet, len, 0, (const sockaddr *)&dest, sizeof(dest));
+  if (r <= 0)
+    ESP_LOGE("pcp", "sendto -> %d (errno %d %s)", r, errno, strerror(errno));
+  else
+    ESP_LOGI("pcp", "sendto -> %d", r);
+}
+
+void PcpClient::queryRouterExternalAddress() {
+  struct PcpPacketExternalAddressRequest p;
+  p.version = p.opcode = 0;
+  memset(&external, 0, sizeof(external));
+
+  sendPacket((const char *)&p, sizeof(p));
+}
+
+in_addr_t PcpClient::getRouterExternalAddress() {
+  return external;
+}
+
+void PcpClient::addPort(int16_t localport, int16_t remoteport, int8_t protocol, int32_t lifetime) {
+  ESP_LOGI("pcp", "PCP addPort %d %d %d %d", (int)localport, (int)remoteport, protocol, lifetime);
+
+  struct PcpPacket	p;
+  memset(&p, 0, sizeof(p));
+
+  p.rh.version = 2;						// PCP
+  p.rh.opcode = PCP_OPCODE_MAP;					// MAP = 1
+  // p.rh.reserved = 0;
+  // p.rh.result_code = 0;
+  p.rh.lifetime = htonl(lifetime);
+  p.rh.client_ip[0] = p.rh.client_ip[1] = 0;
+  p.rh.client_ip[2] = htonl(0x0000FFFF);			// FIXME, looks like "IPv4 follows"
+  p.rh.client_ip[3] = local;					// ESP
+
+  p.mof.nonce[0] = p.mof.nonce[1] = p.mof.nonce[2] = 0;		// FIXME
+  p.mof.protocol = protocol;					// e.g. IPPROTO_TCP; 0 = all protocols
+  p.mof.reserved[0] = p.mof.reserved[1] = p.mof.reserved[2] = 0;
+  p.mof.internal_port = htons(localport);
+  p.mof.external_port = htons(remoteport);
+  // 0 is ok as suggestion for external IP
+  p.mof.external_ip[0] = p.mof.external_ip[1] = p.mof.external_ip[2] = p.mof.external_ip[3] = 0;
+
+  sendPacket((const char *)&p, sizeof(p));
+}
+
+void PcpClient::deletePort(int16_t localport, int8_t protocol) {
+  struct PcpPacket p;
+  memset(&p, 0, sizeof(p));
+
+  p.rh.lifetime = 0;			// Lifetime = 0 means delete
+
+  sendPacket((const char *)&p, sizeof(p));
+}
+
+/* Note for later :
+ * Syntax in a static member function :
+ *  (self ->* ((PcpClient*)self)->PcpClient::catcher)(1, "xx");
+ * Syntax in a member function :
+ *  (this ->* ((PcpClient*)this)->PcpClient::catcher)(1, "xx");
+ *
+ * See also http://stackoverflow.com/questions/990625/c-function-pointer-class-member-to-non-static-member-function
+ */
+/*
+ * This function is not static but a friend, to access private fields and methods in the class.
+ *
+ * Note that RFC 6887 describes a maximum of 1100 octets as a reply packet size.
+ * We're counting on significantly less because we send small requests ourselves.
+ * The union is here to avoid buffer overrun.
+ */
+void pcp_task(void *ptr) {
+  union {
+    struct PcpPacket	pcp;
+    char buffer[128];		// Packets we receive should be smaller than this, see above. Usually 60.
+  } rx;
+  int count = 0;
+
+  while (1) {
+    struct sockaddr_in sender;
+    socklen_t	sl = sizeof(sender);
+
+    if (pcp->sock < 0) {
+      vTaskDelay(2000 / portTICK_PERIOD_MS);
+      continue;
+    }
+    int len = recvfrom(pcp->sock, &rx, sizeof(rx), 0, (struct sockaddr *)&sender, &sl);
+    if (len < 0) {
+      ESP_LOGE("pcp", "Recvfrom failed, errno %d %s, socket %d", errno, strerror(errno), pcp->sock);
+      vTaskDelay(2000 / portTICK_PERIOD_MS);
+      if (count++ > 5)
+        vTaskDelete(0);		// current task
+      continue;
+    } else {
+      ESP_LOGE("pcp", "Received msg len %d", len);
+
+    if ((rx.pcp.rh.opcode & 0x80) == 0) {
+      ESP_LOGE("pcp", "Packet is not a reply, discarding");
+      break;
+    }
+    switch (rx.pcp.rh.opcode & 0x7F) {
+    case PCP_OPCODE_MAP:
+      pcp->PcpReplyMapping(&rx.pcp);
+      break;
+    default:
+      ESP_LOGE("pcp", "Received packet protocol %02x opcode %02x unknown", rx.pcp.rh.version, rx.pcp.rh.opcode);
+      return;
+    }
+#if 1
+  {
+    char s[64], a[10];
+    char *packet = (char *)&rx;
+    for (int i=0; i<len; i++) {
+      if (i % 16 == 0) {
+	if (i > 0) {
+	  ESP_LOGI("pcp", "receive %s", s);
+	  s[0]=0;
+	}
+        sprintf(s, "%02x : ", i);
+      }
+      sprintf(a, "%02x ", packet[i]);
+      strcat(s, a);
+    }
+    if (s[0]) {
+      ESP_LOGI("pcp", "receive %s", s);
+    }
+  }
+#endif
+    }
+
+    vTaskDelay(2000 / portTICK_PERIOD_MS);
+  }
+}
+
+void PcpClient::PcpReplyMapping(struct PcpPacket *rp) {
+  ESP_LOGI("pcp", "ReplyMapping received, decoding ...");
+  // FIXME check nonce ?
+  if (rp->rh.result_code == PCP_RESULT_SUCCESS) {
+    ESP_LOGI("pcp", "Mapping succeeded : int %04x ext %04x, ext ip %s",
+      rp->mof.internal_port, rp->mof.external_port,
+      inet_ntoa(rp->mof.external_ip[3]));
+  } else {
+    ESP_LOGE("pcp", "PCP mapping failed : %s", resultCode2String(rp->rh.result_code));
+  }
+}
+
+char *PcpClient::resultCode2String(int rc) {
+  switch (rc) {
+  case PCP_RESULT_SUCCESS:			return "success";
+  case PCP_RESULT_UNSUPP_VERSION:		return "Unsupported version";
+  case PCP_RESULT_NOT_AUTHORIZED:		return "not authorized";
+  case PCP_RESULT_MALFORMED_REQUEST:		return "malformed request";
+  case PCP_RESULT_UNSUPP_OPCODE:		return "unsupported opcode";
+  case PCP_RESULT_UNSUPP_OPTION:		return "unsupported option";
+  case PCP_RESULT_MALFORMED_OPTION:		return "malformed option";
+  case PCP_RESULT_NETWORK_FAILURE:		return "network failure";
+  case PCP_RESULT_NO_RESOURCES:			return "no resouress";
+  case PCP_RESULT_UNSUPP_PROTOCOL:		return "unsupported protocol";
+  case PCP_RESULT_USER_EX_QUOTA:		return "USER_EX_QUOTA";
+  case PCP_RESULT_CANNOT_PROVIDE_EXTERNAL:	return "CANNOT_PROVIDE_EXTERNAL";
+  case PCP_RESULT_ADDRESS_MISMATCH:		return "address mismatch";
+  case PCP_RESULT_EXCESSIVE_REMOTE_PEERS:	return "excessive remote peers";
+  default:					return "?";
+  }
+}
+
+#if 0
+void PcpClient::catchRouterExternalAddress(uint16_t len, char *data) {
+  if (len == sizeof(PcpPacketExternalAddressReply)) {
+    PcpPacketExternalAddressReply *p = (PcpPacketExternalAddressReply *)data;
+    if (p->opcode != 0x80)
+      return;
+    if (p->version != 0 && p->version != 2)	// NAT-PMP & PCP
+      return;
+    // Looks right now
+    external = p->address;
+  } else
+    return; // silently
+
+  // Success, so switch ourselves off
+}
+#endif
+
+#if 0
+void PcpClient::catchAddPort(uint16_t len, char *data) {
+  if (len == sizeof(PcpPacketMappingReply)) {
+    PcpPacketMappingReply *p = (PcpPacketMappingReply *)data;
+    if (p->opcode != 0x81 && p->opcode != 0x82)
+      return;
+    if (p->version != 0 && p->version != 2)	// NAT-PMP & PCP
+      return;
+    // Looks right now
+    // Nothing to do really
+  } else
+    return; // silently
+}
+#endif
+
+#if 0
+void PcpClient::catchDeletePort(uint16_t len, char *data) {
+  // Nothing to do ?
+
+  // Success, so switch ourselves off
+}
+#endif
---
+++ kippen/esp/main/PcpClient.h
@@ -0,0 +1,149 @@
+/*
+ * Copyright (c) 2017, 2020 Danny Backx
+ *
+ * License (MIT license):
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy
+ *   of this software and associated documentation files (the "Software"), to deal
+ *   in the Software without restriction, including without limitation the rights
+ *   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *   copies of the Software, and to permit persons to whom the Software is
+ *   furnished to do so, subject to the following conditions:
+ *
+ *   The above copyright notice and this permission notice shall be included in
+ *   all copies or substantial portions of the Software.
+ *
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *   THE SOFTWARE.
+ */
+#ifndef _INCLUDE_PCPCLIENT_H_
+#define _INCLUDE_PCPCLIENT_H_
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <esp_event.h>
+
+class PcpClient {
+public:
+  PcpClient();
+  ~PcpClient();
+
+  void setLocalIP(in_addr_t);
+  void setRouter(const char *);
+  void setRouter(const in_addr_t);
+  void queryRouterExternalAddress();
+  in_addr_t getRouterExternalAddress();
+  void addPort(int16_t localport, int16_t remoteport, int8_t protocol, int32_t lifetime);
+  void deletePort(int16_t localport, int8_t protocol);
+
+  esp_err_t NetworkConnected(void *ctx, system_event_t *event);
+  char *resultCode2String(int rc);
+
+private:
+  in_addr_t	local,
+  		external,
+		router_ip;
+  char		*router_name;
+
+  int		sock;
+  friend void pcp_task(void *ptr);	// to access sock
+
+  const int	pcp_client_port = 5350,
+  		pcp_server_port = 5351;
+
+  void sendPacket(const char *packet, const int len);
+  TaskHandle_t task;
+  void PcpReplyMapping(struct PcpPacket *);
+};
+
+struct PcpPacketExternalAddressRequest {
+  uint8_t version;		// 0
+  uint8_t opcode;		// 0
+};
+
+struct PcpPacketExternalAddressReply {
+  uint8_t version;		// 0
+  uint8_t opcode;		// Reply should be 128
+  uint16_t result;
+  uint32_t seconds;
+  uint32_t address;
+};
+
+struct PcpRequestHeader {
+  uint8_t version;		// 2
+  uint8_t opcode;		// 0 for request, 1 for reply
+  uint8_t reserved;
+  uint8_t result_code;		// Actually in request this is also reserved, mandatory 0
+  uint32_t lifetime;		// in seconds
+  uint32_t client_ip[4];
+};
+
+struct PcpOptionsHeader {
+  uint8_t	option_code;
+  uint8_t	reserved;
+  uint16_t	option_length;
+};
+
+struct PcpMappingOpcodeFields {
+  uint32_t	nonce[3];	// 96 bits of nonce
+  uint8_t	protocol;
+  uint8_t	reserved[3];	// 24 bits
+  uint16_t	internal_port;
+  uint16_t	external_port;
+  uint32_t	external_ip[4];	// 128 bits
+};
+
+struct PcpPeerOpcodeFields {
+  uint32_t	nonce[3];	// Mapping nonce, 96 bits
+  uint8_t	protocol;	// Protocol
+  uint8_t	reserved[3];	// Reserved, 24 bits
+  uint16_t	internal_port;	// Internal Port
+  uint16_t	external_port;	// Suggested External Port
+  uint32_t	external_ip[4];	// Suggested External IP Address, 128 bits
+  uint16_t	remote_port;	// Remote Peer Port, 16 bits
+  uint16_t	reserved2;	// Reserved, 16 bits
+  uint32_t	remote_ip[4];	// Remote Peer IP Address, 128 bits
+};
+
+struct PcpPacket {
+  struct PcpRequestHeader	rh;
+  // struct PcpOptionsHeader	mo;
+  struct PcpMappingOpcodeFields	mof;
+};
+
+enum PcpResultCode {
+  PCP_RESULT_SUCCESS = 0,
+  PCP_RESULT_UNSUPP_VERSION,
+  PCP_RESULT_NOT_AUTHORIZED,
+  PCP_RESULT_MALFORMED_REQUEST,
+  PCP_RESULT_UNSUPP_OPCODE,
+  PCP_RESULT_UNSUPP_OPTION,
+  PCP_RESULT_MALFORMED_OPTION,
+  PCP_RESULT_NETWORK_FAILURE,
+  PCP_RESULT_NO_RESOURCES,
+  PCP_RESULT_UNSUPP_PROTOCOL,
+  PCP_RESULT_USER_EX_QUOTA,
+  PCP_RESULT_CANNOT_PROVIDE_EXTERNAL,
+  PCP_RESULT_ADDRESS_MISMATCH,
+  PCP_RESULT_EXCESSIVE_REMOTE_PEERS
+};
+
+enum PcpOpcode {
+  PCP_OPCODE_ANNOUNCE = 0,
+  PCP_OPCODE_MAP = 1,
+  PCP_OPCODE_PEER = 2
+};
+
+enum PcpMapOptionCode {
+  PCP_MAP_OPTION_THIRD_PARTY = 1,
+  PCP_MAP_OPTION_PREFER_FAILURE = 2,
+  PCP_MAP_OPTION_FILTER = 3
+};
+
+extern PcpClient *pcp;
+
+#endif	/* _INCLUDE_PCPCLIENT_H_ */
