0b9f911cd0ef915244885523cc7b2ad087b8329e
dannybackx <dannybackx@users.noreply.github.com>
2021-02-17 06:52:12 +0100

    remove variable

--- ina3221/config.mk
+++ ina3221/config.mk
@@ -11,5 +11,3 @@ ESP_ARDUINO = /home/danny/src/github/esp8266/esp8266-2.7.4/libraries
 LIBS =	../libraries \
 	${ESP_ARDUINO}/ESP8266WebServer \
 	${HOME}/src/github/ina3221
-
-SRC_FILE = measure.cpp
