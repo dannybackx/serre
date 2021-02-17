05f24d2f1ebd7a5f6c536c638127d5bfc12510bc
dannybackx <dannybackx@users.noreply.github.com>
2021-02-17 06:24:53 +0100

    Remove need for link to tools directory

--- switch/config.mk
+++ switch/config.mk
@@ -1,3 +1,6 @@
+# MAKEESPARDUINO_CONFIGS_ROOT =	/home/danny/src/github/makeEspArduino/tools
+__TOOLS_DIR = ${HOME}/src/github/makeEspArduino/tools
+
 BUILD_ROOT = buildroot
 BOARD = d1_mini
 OTA_ADDR = OTA-Switch.local
